06b306808ec2d85b0067f05f1de8b8681fdcbe24
yuuunS2 <dmsgml3505@naver.com>
2019-08-07 17:09:10 +0900

    customer 수정

--- src/customer/CustomerApplyDAO_OracleImpl.java
+++ src/customer/CustomerApplyDAO_OracleImpl.java
@@ -5,142 +5,229 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class CustomerApplyDAO_OracleImpl implements CustomerApplyDAO{
-	
+
+public class CustomerApplyDAO_OracleImpl implements CustomerApplyDAO {
+
 	@Override
 	public void add(CustomerApplyVO vo) throws Exception {
-		
-		List<CustomerApplyVO> ls=new ArrayList<CustomerApplyVO>();
-		Connection conn=null;
-		
-		PreparedStatement stmt=null;
 
-		
-		/*
-		 * 
-		 * create table customer_apply(
-	serialNo varchar2(8) not null, 
-	customer_phone varchar2(11) not null,
-	customer_addr_front varchar2(40) not null , 
-	customer_addr_detail varchar2(45) not null,
-	bag_num number not null, 
-	trash_type number not null,
-	wanted_time varchar2(50) not null, 
-	last_time number not null, 
-	card_num varchar2(20), 
-	helperID varchar2(25),
-	price number,
-	customer_apply_day date not null 
-); 
+		Connection conn = null;
 
-		 * */
-		try{
-		
-			 Class.forName("oracle.jdbc.driver.OracleDriver");
-			 conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521/XE","HR","HR");		
-			 String sql="insert into customer_apply values(?,?,?,?,?,?,?,?,?,?,?)";
-			 stmt=conn.prepareStatement(sql);
-			 stmt.setString(1,vo.getSerialNo());
-			 stmt.setString(2,vo.getCustomer_phone());
-			 stmt.setString(3,vo.getAddr_front());
-			 stmt.setString(4,vo.getHelperID());
-			 stmt.setString(5,vo.getAddr_detail());
-			 stmt.setInt(6,vo.getBag_num());
-			 stmt.setInt(7,vo.getTrash_type());
-			 stmt.setString(8,vo.getTime_wanted());
-			 stmt.setInt(9,vo.getLast_time());
-			 stmt.setInt(10,vo.getPrice());
-			 stmt.setString(11,vo.getCard_num());
-			 int a=stmt.executeUpdate();
-			 System.out.println(a+"d");
-			 conn.commit();
-			 stmt.close();
-			 conn.close();
-			 
-	}catch(Exception e){
-		System.out.println(e.getMessage());
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		int r = -1;
+
+      /*
+   private Integer serialNo;
+   private String customer_phone;
+   private String customer_addr_first;
+   private String customer_addr_second;
+   private String customer_addr_third;
+   private Integer bag_num;
+   private Integer trash_type;
+   private Date wanted_time;
+   private Integer price;
+   private String card_num;
+   private String helperID;
+   private Date customer_apply_day;
+   private Integer certify_status;
+   private Integer review_status;
+       */
+
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521/XE", "HR", "HR");
+			String sql = "select count(*) from customer where customer_phone = ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, vo.getCustomer_phone());
+			rs = stmt.executeQuery();
+
+			if (rs.next()) { // null�� �ƴѰ��, �� 1�� �̻� ��û�� ���� �ִ� ���
+				sql = "update customer set customer_addr_first=?, customer_addr_second=?, customer_addr_third=? where customer_phone = ?";
+				stmt = conn.prepareStatement(sql);
+				stmt.setString(1, vo.getCustomer_addr_first());
+				stmt.setString(1, vo.getCustomer_addr_second());
+				stmt.setString(1, vo.getCustomer_addr_third());
+				r = stmt.executeUpdate();
+				System.out.println("����ȸ�� �ּ��� update"+r);
+			}else {
+				sql = "intsert into customer(customer_addr_first, customer_addr_second, customer_addr_third, customer_phone) values"
+						+ "(?,?,?,?);";
+				stmt = conn.prepareStatement(sql);
+				stmt.setString(1, vo.getCustomer_addr_first());
+				stmt.setString(2, vo.getCustomer_addr_second());
+				stmt.setString(3, vo.getCustomer_addr_third());
+				stmt.setString(4, vo.getCustomer_phone());
+				r = stmt.executeUpdate();
+				System.out.println("�ű�ȸ�� �߰� "+r);
+			}
+
+			sql = "insert into customer_apply(serialNo, customer_phone, customer_addr_first,customer_addr_second,customer_addr_third,"
+					+ "bag_num,trash_type,wanted_time,price,card_num,helperID,customer_apply_day,certify_status,review_status"
+					+ " values(customer_apply_seq.NEXTVAL,?,?,?,?,?,?,null,sysdate,0,0)";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, vo.getCustomer_phone());
+			stmt.setString(2, vo.getCustomer_addr_first());
+			stmt.setString(3, vo.getCustomer_addr_second());
+			stmt.setString(4, vo.getCustomer_addr_third());
+			stmt.setInt(5, vo.getBag_num());
+			stmt.setInt(6, vo.getTrash_type());
+			stmt.setDate(7, new java.sql.Date(vo.getWanted_time().getTime())); //date�� ó���ϴ� ���
+			stmt.setInt(8, vo.getPrice());
+			stmt.setString(9, vo.getCard_num());
+			r = stmt.executeUpdate();
+			System.out.println("insert customer_apply "+r);
+			sql = "select serialNo from customer_apply where customer_phone = ?"
+					+ "order by customer_apply_day desc";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, vo.getCustomer_phone());
+			rs = stmt.executeQuery();
+			int serialNo = rs.getInt("serialNo");
+			System.out.println("�߰��� ��û serialNo "+serialNo);
+
+
+			sql = "select helperID from helper where (wish_addr_first1 = ? and wish_addr_second1 =?)"
+					+ " or (wish_addr_first2 = ? and wish_addr_second2 =?)"
+					+ " or (wish_addr_first3 = ? and wish_addr_second3 =?)";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, vo.getCustomer_addr_first());
+			stmt.setString(2, vo.getCustomer_addr_second());
+			stmt.setString(3, vo.getCustomer_addr_first());
+			stmt.setString(4, vo.getCustomer_addr_second());
+			stmt.setString(5, vo.getCustomer_addr_first());
+			stmt.setString(6, vo.getCustomer_addr_second());
+			rs = stmt.executeQuery();
+			System.out.println("��û���� ���� ������ �°� ��õ�� ��� ���� "+ rs.toString());
+
+			while( rs.next() ) {
+				sql = "insert into matching(serialNo,helperID,suggestion,acceptance) values"
+						+ "(?,?,0,0)";
+				stmt = conn.prepareStatement(sql);
+				stmt.setInt(1, serialNo);
+				stmt.setString(2, rs.getString("HelperID"));
+				r = stmt.executeUpdate();
+			}
+			System.out.println("�����ڵ鿡�� ��õ�� "+ rs.toString());
+
+			conn.commit();
+			stmt.close();
+			conn.close();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
-	}
+
 	@Override
 	public List<CustomerApplyVO> findAll() throws Exception {
+
 		List<CustomerApplyVO> ls=new ArrayList<CustomerApplyVO>();
 		Connection conn=null;
 		Statement stmt=null;
 		ResultSet rs=null;
-		
+
 		try{
-			 Class.forName("oracle.jdbc.driver.OracleDriver");
-			 conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521/XE","HR","HR");
-			 stmt=conn.createStatement();
-			 String sql="select * from customer_apply";
-			 rs=stmt.executeQuery(sql);
-			 while(rs.next()){
-				 CustomerApplyVO vo=new CustomerApplyVO();
-				 vo.setSerialNo(rs.getString(1));
-				 vo.setCustomer_phone(rs.getString(2));
-				 vo.setAddr_front(rs.getString(3));
-				 vo.setHelperID((rs.getString(4)));
-				 vo.setAddr_detail(rs.getString(5));
-				 vo.setBag_num(rs.getString(6));
-				 
-				 vo.setTrash_type(rs.getString(7).split(","));
-				 vo.setTime_wanted(rs.getString(8).split(","));
-				 vo.setLast_time(rs.getString(9));
-				 vo.setPrice(rs.getString(10));
-				 vo.setCard_num(rs.getString(11));
-				 ls.add(vo);
-			 }
-			 rs.close();
-	
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521/XE","HR","HR");
+			stmt=conn.createStatement();
+			String sql="select * from customer_apply";
+			rs=stmt.executeQuery(sql);
+			while(rs.next()){
+				CustomerApplyVO vo=new CustomerApplyVO();
+				vo.setSerialNo(rs.getInt(1));
+				vo.setCustomer_phone(rs.getString(2));
+				vo.setCustomer_addr_first(rs.getString(3));
+				vo.setCustomer_addr_second(rs.getString(4));
+				vo.setCustomer_addr_third(rs.getString(5));
+				vo.setBag_num(rs.getInt(6));
+				vo.setTrash_type(rs.getInt(7));
+				vo.setWanted_time(rs.getDate(8));
+				vo.setPrice(rs.getInt(9));
+				vo.setCard_num(rs.getString(10));
+				vo.setHelperID(rs.getString(11));
+				vo.setCustomer_apply_day(rs.getDate(12));
+				ls.add(vo);
+			}
+			rs.close();
+
 		}catch(Exception e){
 		}
 		finally{
 			try {
-		         if (stmt != null )stmt.close();
-		         if (conn != null )conn.close();
-		         } catch (Exception e) {
-		         }
-			
+				if (stmt != null)
+					stmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+
 		}
 		return ls;
-	
+
 	}
+
 	@Override
-	public void update(String phone,String price,String card_num) throws Exception
+	public void update(String phone,int price,String card_num) throws Exception
 	{
-		
+
 		Connection conn=null;
-		
+
 		PreparedStatement stmt=null;
-		
+		PreparedStatement stmt1=null;
+
 		try{
 
-			 Class.forName("oracle.jdbc.driver.OracleDriver");
-			 conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521/XE","HR","HR");
-			 String sql="update customer_apply set price=?,card_num=? where customer_phone=?";
-			 
-			 stmt=conn.prepareStatement(sql);			
-			 stmt.setString(1, price);
-			 stmt.setString(2,card_num);
-			 stmt.setString(3,phone);
-			 int a=stmt.executeUpdate();
-			 System.out.println(a+"d");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521/XE","HR","HR");
+			String sql="update customer_apply set price=?,card_num=? where customer_phone=?";
+			String sql1="select count(*) from customer where customer_phone = ?";
+			stmt1=conn.prepareStatement(sql1);
+			ResultSet rs1=stmt1.executeQuery();
+			//업데이트
+			if(rs1.next()){
+				String s="update customer set customer_addr_first=?, customer_addr_second=?, customer_addr_third=? where customer_phone = ?";
+				PreparedStatement imsi=null;
+				imsi.setString(1,rs1.getString(3));
+				imsi.setString(2,rs1.getString(4));
+				imsi.setString(3,rs1.getString(5));
+				int a=imsi.executeUpdate();
+				System.out.println(a);
+				imsi.close();
+			}
+			//추가
+			else{
+				String s="insert into customer values(?,?,?,?)";
+				PreparedStatement imsi1=null;
+				imsi1=conn.prepareStatement(sql);
+				imsi1.setInt(1, price);
+				imsi1.setString(2,card_num);
+				imsi1.setString(3,phone);
+				int a=imsi1.executeUpdate();
+				System.out.println(a+"d");
+				imsi1.close();
+			}
+			stmt.setInt(1,price);
+			stmt.setString(2,card_num);
+			stmt.setString(3,phone);
+
 		}catch(Exception e){
 		}
-		finally{
-			try {
-		         if (stmt != null )stmt.close();
-		         if (conn != null )conn.close();
-		         } catch (Exception e) {
-		         }
-			
+		finally{try {
+			if (stmt != null)
+				stmt.close();
+			if (conn != null)
+				conn.close();
+		} catch (Exception e) {
 		}
-	
-		
+
+		}
+
 	}
 
 }
-
