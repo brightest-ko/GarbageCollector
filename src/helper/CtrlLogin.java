1b6771d2be6d03ef0efca84b8241155dab24d8b8
yuuunS2 <dmsgml3505@naver.com>
2019-08-08 12:22:27 +0900

    HelperSignUpDAO 로그인 체크 메소드 추가

---
+++ src/helper/CtrlLogin.java
@@ -0,0 +1,38 @@
+package helper;
+
+import common.Controller;
+import common.RequestMapping;
+import common.Util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@RequestMapping("/helper/login.do")
+public class CtrlLogin implements Controller {
+    @Override
+    public String handleRequest(HttpServletRequest request,
+                                HttpServletResponse response) throws Exception {
+        String id = request.getParameter("helperId");
+        String password = request.getParameter("pw");
+        String result;
+
+        System.out.println("input id : " +  id + " input pw :" + password);
+
+        HelperSignUpDAO dao = new HelperSignUpDAO_OracleImpl();
+        if (dao.checkLoginInfo(id, password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("auth", "helper");
+            session.setAttribute("helperId", id);
+            result = "SUCCESS";
+        }
+        else {
+
+            result = "FAIL";
+        }
+        System.out.println("44444444");
+        System.out.println(result);
+        return "redirect:/index.do";
+    }
+}
